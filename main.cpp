37c03a4993232788f8de84c28a7217e36007bdda
Lubos Zapotocny <zapotocnylubos@gmail.com>
2023-03-06 15:08:36 +0100

    WIP without cut

--- main.cpp
+++ main.cpp
@@ -28,50 +28,82 @@ struct Graph {
 
         infile.close();
     }
-};
 
-int min = std::numeric_limits<int>::max();
+    [[nodiscard]] int cutWeight(const std::vector<bool> &cut) const {
+        int weight = 0;
+        for (int i = 0; i < size; i++) {
+            if (!cut[i]) continue;
 
-void DFS_BB(const Graph &g, int maxPartitionSize, std::vector<bool> &vec, int index, int count) {
-    // Pokud jsme vygenerovali tři true hodnoty, vypíšeme aktuální kombinaci
-    if (count == maxPartitionSize) {
-        for (auto &&i: vec) {
-            std::cout << i << " ";
-        }
-        std::cout << " -> ";
-        int sum = 0;
-        for (int i = 0; i < g.size; i++) {
-            if (!vec[i]) continue;
-
-            for (int j = 0; j < g.size; j++) {
-                if (vec[j]) continue;
-                sum += g.graph[i][j];
+            for (int j = 0; j < size; j++) {
+                if (cut[j]) continue;
+                weight += graph[i][j];
             }
         }
-        if (sum < min) {
-            min = sum;
+        return weight;
+    }
+};
+
+void DFS_BB(const Graph &graph, int maxPartitionSize,
+            std::vector<bool> &cut, int index, int count, int &bestWeight) {
+    // end stop of the recursion
+    if (index > graph.size) {
+        return;
+    }
+
+    // this cannot be better solution
+//    if (currentWeight >= bestWeight) {
+//        return;
+//    }
+
+    if (count == maxPartitionSize) {
+        // count this vertex cut
+        int currentWeight = graph.cutWeight(cut);
+
+        if (currentWeight < bestWeight) {
+            bestWeight = currentWeight;
         }
-        std::cout << sum << std::endl;
+
+        for (auto i: cut) {
+            std::cout << i << " ";
+        }
+
+        std::cout << " -> " << currentWeight << std::endl;
         return;
     }
 
-    // Pokud jsme na konci vektoru, neděláme nic
-    if (index == vec.size()) {
-        return;
-    }
+    // try without this vertex
+    DFS_BB(graph, maxPartitionSize, cut, index + 1, count, bestWeight);
 
-    // Nastavíme aktuální prvek na true a zavoláme rekurzivně funkci pro další index s počtem true hodnot o jedna větším
-    vec[index] = true;
-    DFS_BB(g, maxPartitionSize, vec, index + 1, count + 1);
+    // try with this vertex
+    cut[index] = true;
 
-    // Nastavíme aktuální prvek na false a zavoláme rekurzivně funkci pro další index s počtem true hodnot stejným
-    vec[index] = false;
-    DFS_BB(g, maxPartitionSize, vec, index + 1, count);
+    // try with this vertex
+    DFS_BB(graph, maxPartitionSize, cut, index + 1, count + 1, bestWeight);
+
+    // restore the status of the cut
+    cut[index] = false;
 }
 
-void DFS_BB(const Graph &g, int maxPartitionSize) {
+int DFS_BB(const Graph &g, int maxPartitionSize) {
     auto cut = std::vector(g.size, false);
-    DFS_BB(g, maxPartitionSize, cut, 0, 0);
+//    for (int i = 0; i < maxPartitionSize - 1; i++) {
+//        cut[i] = true;
+//    }
+//
+//    int weight = 0;
+//    for (int i = 0; i < g.size; i++) {
+//        for (int j = 0; j < g.size; j++) {
+//            if (cut[j]) {
+//                continue;
+//            }
+//            weight += g.graph[i][j];
+//        }
+//    }
+
+//    int bestWeight = weight;
+    int bestWeight = std::numeric_limits<int>::max();
+    DFS_BB(g, maxPartitionSize, cut, 0, 0, bestWeight);
+    return bestWeight;
 }
 
 int main(int argc, char **argv) {
@@ -87,8 +119,8 @@ int main(int argc, char **argv) {
 
     auto cut = std::vector<bool>(g.size, false);
 
-    DFS_BB(g, maxPartitionSize);
+    int bestWeight = DFS_BB(g, maxPartitionSize);
 
-    std::cout << "Global minimum is: " << min << std::endl;
+    std::cout << "Global minimum is: " << bestWeight << std::endl;
     return 0;
 }
